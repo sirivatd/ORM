998ec83fbf0d104d9efaa07098d663f8b305a745
sirivat <dsirivat17@gmail.com>
2018-09-03 23:51:04 -0700

    implement Play orm

---
+++ plays.rb
@@ -0,0 +1,21 @@
+require 'sqlite3'
+require 'singleton'
+
+class PlayDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('plays.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Play
+  attr_accessor :title, :year, :playwright_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Play.new(datum) }
+  end
+end
